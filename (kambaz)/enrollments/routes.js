d20bffe35c2df91c5cd755a7b72b7123d7e024eb
LondonJones <londonjonez00@gmail.com>
2026-04-12 17:55:51 -0400

    enrollments

--- (kambaz)/enrollments/routes.js
+++ (kambaz)/enrollments/routes.js
@@ -1,32 +1,31 @@
 import EnrollmentsDao from "./dao.js"
-
 export default function EnrollmentsRoutes(app) {
-    const dao = EnrollmentsDao();
+  const dao = EnrollmentsDao();
 
-    const enrollInCourse = async (req, res) => {
-        const { uid, cid } = req.params;
+  const findAllEnrollments = async (req, res) => {
+    const enrollments = await dao.findAllEnrollments();
+    res.json(enrollments);
+  };
 
-        if (uid === "current") {
-            const currentUser = req.session["currentUser"];
-            uid = currentUser._id;
-        }
-        const status = await enrollmentsDao.enrollUserInCourse(uid, cid);
-        res.send(status);
-
+  const enrollInCourse = async (req, res) => {
+    let { uid, cid } = req.params;
+    if (uid === "current") {
+      uid = req.session["currentUser"]._id;
     }
+    const status = await dao.enrollUserInCourse(uid, cid);
+    res.send(status);
+  };
 
-    const unenrollInCourse = async (req, res) => {
-        const { uid, cid } = req.params;
-
-        if (uid === "current") {
-            const currentUser = req.session["currentUser"];
-            uid = currentUser._id;
-        }
-        const status = await enrollmentsDao.unenrollUserFromCourse(uid, cid);
-        res.send(status);
-
+  const unenrollInCourse = async (req, res) => {
+    let { uid, cid } = req.params;
+    if (uid === "current") {
+      uid = req.session["currentUser"]._id;
     }
+    const status = await dao.unenrollUserFromCourse(uid, cid);
+    res.send(status);
+  };
 
-    app.post("/api/courses/:courseId/enroll", enrollInCourse);
-    app.delete("/api/courses/:courseId/enroll", unenrollInCourse);
+  app.get("/api/enrollments", findAllEnrollments);
+  app.post("/api/courses/:courseId/enroll", enrollInCourse);
+  app.delete("/api/courses/:courseId/enroll", unenrollInCourse);
 }
