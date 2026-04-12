744c204c1e961db61ca3fd2b2e79c0792e0535f6
LondonJones <londonjonez00@gmail.com>
2026-04-12 18:09:07 -0400

    enrollments

--- (kambaz)/enrollments/dao.js
+++ (kambaz)/enrollments/dao.js
@@ -1,24 +1,24 @@
 import { v4 as uuidv4 } from "uuid";
 import model from "./model.js"
-
 export default function EnrollmentsDao() {
+  const findAllEnrollments = () => model.find();
 
   async function findCoursesForUser(userId) {
     const enrollments = await model.find({ user: userId }).populate("course");
     return enrollments.map((enrollment) => enrollment.course);
   }
+
   async function findUsersForCourse(courseId) {
     const enrollments = await model.find({ course: courseId }).populate("user");
     return enrollments.map((enrollment) => enrollment.user);
   }
- 
+
   function enrollUserInCourse(userId, courseId) {
     return model.create({
       user: userId,
       course: courseId,
       _id: `${userId}-${courseId}`,
     });
- 
   }
 
   function unenrollUserFromCourse(user, course) {
@@ -28,7 +28,6 @@ export default function EnrollmentsDao() {
   function unenrollAllUsersFromCourse(courseId) {
     return model.deleteMany({ course: courseId });
   }
- 
 
-  return { enrollUserInCourse, unenrollUserFromCourse, findCoursesForUser, findUsersForCourse, unenrollAllUsersFromCourse };
+  return { findAllEnrollments, enrollUserInCourse, unenrollUserFromCourse, findCoursesForUser, findUsersForCourse, unenrollAllUsersFromCourse };
 }
